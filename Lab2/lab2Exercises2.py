a5b39597d0fd3b99a6206984bf994cdebffca2f5
Thomas Bercasio <relempagotb@gmail.com>
2018-09-11 17:17:30 -0700

    bunch of changes hope it works
    ^^^

--- Lab2/lab2Exercises2.py
+++ Lab2/lab2Exercises2.py
@@ -1,8 +1,8 @@
 num = eval(input("What is your number: "))
 
-first = int(num)%1000
-second = int(num)%100
-third = int(num)%10
-fourth = int(num)%1
+first = int(num) % 1000
+second = int(num) % 100
+third = int(num) % 10
+fourth = int(num) % 1
 
-print(first, second, third, fourth)
+print("%d\n %d\n %d\n %d\n", first, second, third, fourth)
