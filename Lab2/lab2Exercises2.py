0dc2851556c796bd54fd98c87783d187172aefa4
Thomas Bercasio <relempagotb@gmail.com>
2018-09-11 17:33:22 -0700

    Yep this is a shitty solution
    but a solution nonetheless

--- Lab2/lab2Exercises2.py
+++ Lab2/lab2Exercises2.py
@@ -1,8 +1,15 @@
 num = eval(input("What is your number: "))
 
-first = int(num) % 1000
+first = int(num) % 10
+num -= first
+first /= 1
 second = int(num) % 100
-third = int(num) % 10
-fourth = int(num) % 1
+num -= second
+second /= 10
+third = int(num) % 1000
+num -= third
+third /= 100
+fourth = int(num) % 10000
+fourth /= 1000
 
-print("%d\n %d\n %d\n %d\n", first, second, third, fourth)
+print("%d\n%d\n%d\n%d\n" % (first, second, third, fourth))
