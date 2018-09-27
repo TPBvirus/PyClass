b55904929a71c0dfb028f10ad3db566e41094129
Thomas Pascual Bercasio <relempagotb@gmail.com>
2018-09-27 12:58:11 -0700

    format changes
    ^^^

--- Lab 5/lab5_29.py
+++ Lab 5/lab5_29.py
@@ -1,11 +1,21 @@
-for x in range(2001, 2101):
-    if x % 4 == 0:
-        print(x , end = ' ')
+for x in range(2001, 2101, 50):
+    for y in range (0,50):
+        z = y+x
+        if z % 4 == 0:
+            if z % 100 != 0:
+                print(z, end = ' ')
+    print()
 
 print()
 
 i = 2001
 while i <= 2100:
-    if i % 4 == 0:
-        print(i , end = ' ')
-    i += 1
+    j = 0
+    while j <= 50:
+        k = i+j
+        if k % 4 == 0:
+            if k % 100 != 0:
+                print(k , end = ' ')
+        j += 1
+    print()
+    i += 50
